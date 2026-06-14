4ad21b3e2c8c4ae2738c6fa34aff32b090c33675
hikidy <138530988+hikidy@users.noreply.github.com>
2026-06-14 17:07:54 -0400

    Add files via upload

---
+++ maps/petell.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="petell" tilewidth="240" tileheight="240" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0" x="0" y="0" width="120" height="120">
+  <image source="costume4 (2).png" width="240" height="240"/>
+ </tile>
+ <tile id="1">
+  <image source="costume4 (3).png" width="240" height="240"/>
+ </tile>
+ <tile id="2">
+  <image source="costume4 (2).png" width="240" height="240"/>
+ </tile>
+</tileset>
